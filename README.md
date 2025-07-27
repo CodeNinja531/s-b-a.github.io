2eb1c6e6383b8fb3efc045c5bedb62e6617eedef
CodeNinja531 <s20201033@carmelss.edu.hk>
2025-07-27 19:22:23 +0800

    menu

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # s-b-a.github.io
 just for my S5 ICT SBA (and this is the page:
 https://codeninja531.github.io/s-b-a.github.io/
+https://bit.ly/sbasports
 
 This is a website for helping with the school Sports day
+
 Current problems:
 The database function is not fully implemented
--- globalVariables.js
+++
@@ -1,8 +0,0 @@
-// globalVariables.js
-// Centralized file for all global variables
-
-// Example global variables
-export const API_BASE_URL = 'https://api.example.com';
-export const DEFAULT_LANGUAGE = 'en';
-export const THEME_COLOR = '#3498db';
-// Add more global variables as needed
